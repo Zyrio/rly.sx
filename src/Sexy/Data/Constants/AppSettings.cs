bc2c2c07e30bab4ef81ee71c7e8d4f4af45965e9
Josh Robertson <josh.robertson@legendware.co.uk>
2017-04-04 16:26:36 +0100

    Implement file limit for upload

--- src/Sexy/Data/Constants/AppSettings.cs
+++ src/Sexy/Data/Constants/AppSettings.cs
@@ -6,6 +6,7 @@ namespace Sexy.Data.Constants
         public static string FilenameLength { get; set; }
         public static string InstanceName { get; set; }
         public static string InstanceProtocol { get; set; }
+        public static string MaxFilesize { get; set; }
         public static string UploadStorageEndpoint { get; set; }
         public static string UploadStoragePath { get; set; }
     }
