4c870080fdc1255430159f389256e6dc2ed57005
Josh Robertson <josh.robertson@legendware.co.uk>
2017-04-05 11:10:04 +0100

    Bump version to 17.02.0

--- src/Sexy/Data/Constants/Version.cs
+++ src/Sexy/Data/Constants/Version.cs
@@ -2,8 +2,8 @@
 namespace Sexy {
     public class Version
     {
-        public static int Release = 1701;
-        public static int Patch = 2;
+        public static int Release = 1702;
+        public static int Patch = 0;
         public static string Codename = "";
     }
 }
