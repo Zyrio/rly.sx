b71b180db10f684ee1ed93472f2f9ce0aa2d88e2
Ducky <ducky.vexton@outlook.com>
2017-09-30 21:45:16 +0100

    Remove useless stuff

--- build/build.application.sh
+++
@@ -1,7 +0,0 @@
-cd src/Sexy    # set directory
-dotnet restore # install NuGet packages
-cd ../..       # reset directory
-
-cd src/Sexy    # set directory
-dotnet build   # build project
-cd ../..       # reset directory
--- build/build.database.sh
+++
@@ -1,3 +0,0 @@
-cd src/Sexy               # set directory
-dotnet ef database update # update database
-cd ../..                  # reset directory
