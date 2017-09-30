5eb020b2e1b0bb047b206efe65b801f6b3aa1a99
Ducky <ducky.vexton@outlook.com>
2017-09-30 22:12:52 +0100

    Bump version to 1703.1 (17.3.1)

--- src/Sexy/Data/Constants/VersionConstant.cs
+++ src/Sexy/Data/Constants/VersionConstant.cs
@@ -3,7 +3,7 @@ namespace Sexy.Data.Constants {
     public class VersionConstant
     {
         public static int Release = 1703;
-        public static int Patch = 0;
+        public static int Patch = 1;
         public static string Codename = "";
         public static bool Unstable = false;
     }
